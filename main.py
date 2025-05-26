b252c7f80acf5e4c9ef3faa63c0891f6e98317d1
GrandpaRex <tomhenkel95@gmail.com>
2025-05-26 15:14:35 -0500

    Added the proper checks and outputs

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from stats import number_of_characters
+from stats import init
 
 def main():
-    result = number_of_characters("books/frankenstein.txt")
-    print(result)
+    book_path = "books/frankenstein.txt"
+    word_count, letter_list = init(book_path)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for char_dict in letter_list:
+        print(f"{char_dict["char"]}: {char_dict["num"]}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,11 +1,15 @@
 def get_book_text(file_path):
     with open(file_path) as f:
         file_contents = f.read()
-        return file_contents
+    return file_contents
 
-def number_of_characters(filepath):
-    result = get_book_text(filepath).lower()
+def sort_on(dict):
+    return dict["num"]
+
+def number_of_characters(book_string):
+    result = book_string.lower()
     letter_dictionary = {}
+    dictionary_list = []
     for words in result:
         if words.isalpha():
             if words in letter_dictionary:
@@ -13,4 +17,20 @@ def number_of_characters(filepath):
                 letter_dictionary[words] = new_value
             else:
                 letter_dictionary[words] = 1
-    return letter_dictionary
+    for letter in letter_dictionary:
+        dictionary_list.append({"char": letter, "num": letter_dictionary[letter]})
+    dictionary_list.sort(reverse=True, key=sort_on)
+    return dictionary_list
+
+def number_of_words(book_string):
+    result = book_string.split()
+    number_of_words = 0
+    for words in result:
+        number_of_words += 1
+    return number_of_words
+
+def init(file_path):
+    result = get_book_text(file_path)
+    word_count = number_of_words(result)
+    letter_dictionary = number_of_characters(result)
+    return word_count, letter_dictionary
