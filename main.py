8e1819f63bee7ce607f49fe5af9fd12e6aeda715
GrandpaRex <tomhenkel95@gmail.com>
2025-05-26 15:25:04 -0500

    Remove hard coded book path and add import logic

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import sys
 from stats import init
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     word_count, letter_list = init(book_path)
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {book_path}")
